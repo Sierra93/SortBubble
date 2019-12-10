6947042aedb1fce152838fddd9bc3bb1c873bfe6
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-12-11 01:45:38 +0300

    Добавьте файлы проекта.

---
+++ SortBubble/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SortBubble {
+    class Program {
+        static void Main() {
+            int[] array = new int[10];
+            Random r = new Random();
+            Console.WriteLine("Начальный массив: ");
+            for (int i = 0; i < array.Length; i++) {
+                array[i] = r.Next(1, 10);
+                Console.WriteLine("Элемент [" + i + "]: " + array[i]);
+            }
+            Console.WriteLine("Новый массив:");
+            for (int i = 0; i < array.Length; i++) {
+                for (int j = i + 1; j < array.Length; j++) {
+                    if (array[i] > array[j]) {
+                        int tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+                Console.WriteLine("Элемент [" + i + "]: " + array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
